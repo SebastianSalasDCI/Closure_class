5b2a9b5b5af8927f2a7d90c76eec0cd5d8473051
Tobias Pfaffenzeller <tobler@gmx.com>
2021-07-14 15:23:33 +0200

    solution Tobias

--- solution.js
+++ solution.js
@@ -1,1 +1,14 @@
 //Solution exercise 4.
+function createBase(x){
+    let base = x;
+    function adder(y){
+        return base + y
+    }
+    return adder
+  }
+  
+const addSix = createBase(6);
+const addTwelve = createBase(12);
+  
+console.log(addSix(10));  
+console.log(addTwelve(20)); 
